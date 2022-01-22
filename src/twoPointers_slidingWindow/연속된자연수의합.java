3222bce6eae90b652fa1510ae4f2c67d48a5631d
seongwoo-choi <hibogo789@gmail.com>
2022-01-22 20:49:45 +0900

    fix:연속된 자연수의 합

--- src/twoPointers_slidingWindow/연속된자연수의합.java
+++ src/twoPointers_slidingWindow/연속된자연수의합.java
@@ -1,23 +1,25 @@
 package twoPointers_slidingWindow;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class 연속된자연수의합 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
+
+
         int answer = 0, sum = 0, lt = 0;
         int[] arr = new int[n];
-        for (int i = 1; i <= n; i++) {
-            arr[i - 1] = i;
+        int m = n / 2 + 1;
+
+        for (int i = 0; i <= m; i++) {
+            arr[i] = i + 1;
         }
-        for (int rt = 0; rt < n; rt++) {
+
+        for (int rt = 0; rt < m; rt++) {
             sum += arr[rt];
             if (sum == n) answer++;
-
             while (sum >= n) {
-                if (rt == n - 1) break;
                 sum -= arr[lt++];
                 if (sum == n) answer++;
             }
---
+++ src/twoPointers_slidingWindow/최대길이연속부분수열.java
@@ -0,0 +1,9 @@
+package twoPointers_slidingWindow;
+
+import java.util.Scanner;
+
+public class 최대길이연속부분수열 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+    }
+}
