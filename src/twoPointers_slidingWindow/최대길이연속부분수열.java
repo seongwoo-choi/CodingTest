a39644b97b7f3b9c8a08ca0b5556333db645f895
seongwoo-choi <hibogo789@gmail.com>
2022-01-22 22:27:09 +0900

    최대길이 연속 부분 수열(런타임 에러..fix 해야함)

--- src/twoPointers_slidingWindow/최대길이연속부분수열.java
+++ src/twoPointers_slidingWindow/최대길이연속부분수열.java
@@ -5,5 +5,49 @@ import java.util.Scanner;
 public class 최대길이연속부분수열 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
+        int[] a = new int[2];
+        for (int i = 0; i < 2; i++) {
+            a[i] = kb.nextInt();
+        }
+        int n = a[0], k = a[1];
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        int answer = 0, max = Integer.MIN_VALUE, rt = 0, lt = 0, cnt = 0;
+        while (true) {
+            if (cnt == k) {
+                if (arr[rt] == 1) {
+                    answer++;
+                    rt++;
+                    max = Math.max(max, answer);
+                    continue;
+                }
+                if (arr[rt] == 0) {
+                    answer = 0;
+                    cnt = 0;
+                    rt = lt++;
+                    continue;
+                }
+            }
+            if (cnt < k + 1) {
+                if (arr[rt] == 1) {
+                    answer++;
+                    rt++;
+                    max = Math.max(max, answer);
+                }
+                if (arr[rt] == 0) {
+                    answer++;
+                    cnt++;
+                    rt++;
+                    max = Math.max(max, answer);
+                }
+            }
+            if (rt == n-1) break;
+        }
+
+        System.out.println(max);
+
     }
 }
