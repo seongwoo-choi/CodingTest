a49428df5e2544c7165d7af8b1e499161bc67536
seongwoo-choi <hibogo789@gmail.com>
2022-01-26 20:52:45 +0900

    fix: 아나그램 (해시맵 사용)

--- src/hashMap_treeSet/아나그램_해쉬.java
+++ src/hashMap_treeSet/아나그램_해쉬.java
@@ -1,42 +1,29 @@
 package hashMap_treeSet;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class 아나그램_해쉬 {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
-        String a = kb.nextLine();
-        String b = kb.nextLine();
-        char[] aa = new char[a.toCharArray().length];
-        char[] bb = new char[b.toCharArray().length];
-        boolean check = false;
+        String s1 = kb.nextLine();
+        String s2 = kb.nextLine();
 
-        for (int i=0; i<a.toCharArray().length; i++) {
-            aa[i] = a.toCharArray()[i];
+        String answer = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char x : s1.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
         }
 
-        for (int i=0; i<b.toCharArray().length; i++) {
-            bb[i] = b.toCharArray()[i];
-        }
-
-        Arrays.sort(aa);
-        Arrays.sort(bb);
-
-        for (int i=0; i<aa.length; i++) {
-            if(aa[i] != bb[i]) {
-                check = false;
+        for (char x : s2.toCharArray()) {
+            if (!map.containsKey(x) || map.get(x) == 0) {
+                answer = "NO";
                 break;
-            } else {
-                check = true;
             }
-        }
-        if (check) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
+            map.put(x, map.get(x) - 1);
         }
 
-
+        System.out.println(answer);
     }
 }
