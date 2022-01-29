83e08b1d71ea80cd196912caf62b5dc9d0991efc
seongwoo-choi <hibogo789@gmail.com>
2022-01-29 21:51:38 +0900

    크레인 인형 뽑기

---
+++ src/stact_queue/크레인_인형뽑기.java
@@ -0,0 +1,40 @@
+package stact_queue;
+
+import java.util.*;
+
+public class 크레인_인형뽑기 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = kb.nextInt();
+            }
+        }
+        int m = kb.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) moves[i] = kb.nextInt();
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int pos : moves) {
+            for (int i = 0; i < n; i++) {
+                if (board[i][pos - 1] != 0) {
+                    int tmp = board[i][pos - 1];
+                    board[i][pos - 1] = 0;
+                    if (!stack.isEmpty() && tmp == stack.peek()) {
+                        answer += 2;
+                        stack.pop();
+                    }
+                    else stack.push(tmp);
+                    // break 를 안하면 선택된 열의 모든 행을 전부 탐색한다.
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
