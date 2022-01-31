def9d829a13f3e97655735bae8215f8af3b07664
seongwoo-choi <hibogo789@gmail.com>
2022-01-31 18:53:38 +0900

    선택 정렬

---
+++ src/sorting_searching/선택_정렬.java
@@ -0,0 +1,26 @@
+package sorting_searching;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 선택_정렬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int x:arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
