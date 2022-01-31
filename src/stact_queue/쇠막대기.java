ffd1e8b2174b5fa3b10f5b29456a78e54a64ebaf
seongwoo-choi <hibogo789@gmail.com>
2022-01-31 14:03:04 +0900

    쇠막대기

--- src/stact_queue/쇠막대기.java
+++ src/stact_queue/쇠막대기.java
@@ -1,7 +1,24 @@
 package stact_queue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class 쇠막대기 {
     public static void main(String[] args) {
-
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') stack.push('(');
+            else {
+                // 레이저
+                stack.pop();
+                if (str.charAt(i-1) == '(') answer += stack.size();
+                // 쇠막대기
+                else answer++;
+            }
+        }
+        System.out.println(answer);
     }
 }
