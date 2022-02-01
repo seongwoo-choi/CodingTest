4f40c83886ee279f086d45e884be34a462cc8817
seongwoo-choi <hibogo789@gmail.com>
2022-02-01 17:55:13 +0900

    fix: 선택 정렬

--- src/sorting_searching/선택_정렬.java
+++ src/sorting_searching/선택_정렬.java
@@ -11,13 +11,18 @@ public class 선택_정렬 {
         for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
 
         for (int i = 0; i < arr.length; i++) {
+            int idx = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+                if(arr[j] < arr[idx]) idx=j;
+//                if (arr[i] > arr[j]) {
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
             }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
         }
         for (int x:arr) {
             System.out.print(x + " ");
