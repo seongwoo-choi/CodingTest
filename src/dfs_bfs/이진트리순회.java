0eb3db08d5d3dbce57aafc02cb25f02c38b0f210
seongwoo-choi <hibogo789@gmail.com>
2022-02-04 22:42:33 +0900

    DFS_부분집합구하기

--- src/dfs_bfs/이진트리순회.java
+++ src/dfs_bfs/이진트리순회.java
@@ -2,25 +2,17 @@ package dfs_bfs;
 
 import java.util.*;
 
-class Node {
-    int data;
-    Node lt, rt;
-    public Node(int val) {
-        data = val;
-        lt=rt=null;
-    }
-}
-
 public class 이진트리순회 {
     Node root;
+
     public void DFS(Node root) {
         // 말단 노드
-        if(root == null) return;
-        // 말단 노드가 될 때 까지 재귀를 돌린다.
-        // 왼쪽과 오른쪽으로 뻗는 케이스 두가지를 지정
+        if (root == null) return;
+            // 말단 노드가 될 때 까지 재귀를 돌린다.
+            // 왼쪽과 오른쪽으로 뻗는 케이스 두가지를 지정
         else {
             // 전위 순회
-//            System.out.println(root.data);
+            System.out.println(root.data);
             DFS(root.lt);
             // 중위 순회
 //            System.out.println(root.data);
@@ -40,5 +32,15 @@ public class 이진트리순회 {
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
         tree.DFS(tree.root);
+    }
+
+    private static class Node {
+        int data;
+        Node lt, rt;
+
+        public Node(int val) {
+            data = val;
+            lt = rt = null;
+        }
     }
 }
