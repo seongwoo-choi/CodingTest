bde9ef1035cdc38999320f6897fcdabd13a1a4d0
seongwoo-choi <hibogo789@gmail.com>
2022-02-05 13:20:34 +0900

    BFS_이진트리순회

---
+++ src/dfs_bfs/이진트리순회_BFS.java
@@ -0,0 +1,48 @@
+package dfs_bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 이진트리순회_BFS {
+    Node root;
+
+    private static class Node {
+        int data;
+        Node lt, rt;
+
+        public Node(int val) {
+            data = val;
+            lt = rt = null;
+        }
+    }
+
+    public void BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            System.out.println(L + " ");
+            for (int i = 0; i < len; i++) {
+                Node cur = Q.poll();
+                System.out.print(cur.data + " ");
+                if(cur.lt != null) Q.offer(cur.lt);
+                if(cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        이진트리순회_BFS tree = new 이진트리순회_BFS();
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
+        tree.BFS(tree.root);
+    }
+}
