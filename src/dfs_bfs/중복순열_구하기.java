272d32c6553fc405012b8c20f8e552e50e7d7df5
seongwoo-choi <hibogo789@gmail.com>
2022-02-11 20:47:23 +0900

    중복순열 구하기 DFS

--- src/dfs_bfs/중복순열_구하기.java
+++ src/dfs_bfs/중복순열_구하기.java
@@ -1,8 +1,30 @@
 package dfs_bfs;
 
+import java.util.Scanner;
+
 public class 중복순열_구하기 {
 
-    public static void main(String[] args) {
+    static int n, m;
+    static int[] pm;
 
+    public void DFS(int L) {
+        if (L == m) {
+            for(int x:pm) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for(int i=1; i<=n; i++) {
+                pm[L] = i;
+                DFS(L+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        중복순열_구하기 T = new 중복순열_구하기();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        pm = new int[m];
+        T.DFS(0);
     }
 }
