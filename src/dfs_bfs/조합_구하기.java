7558f22a9c5fe1ae27f1c599a09ab25a3e153839
seongwoo-choi <hibogo789@gmail.com>
2022-02-17 20:38:19 +0900

    fix:조합 구하기 DFS!!

--- src/dfs_bfs/조합_구하기.java
+++ src/dfs_bfs/조합_구하기.java
@@ -5,30 +5,18 @@ import java.util.*;
 public class 조합_구하기 {
 
     static int n, m;
-    static int[] ch, arr;
-    static int[][] ch2;
-    static ArrayList<Integer> b = new ArrayList<>();
+    static int[] combi;
 
-    public int DFS(int L, int[] arr) {
-        if(ch2[arr[0]][arr[1]] > 0) return ch2[arr[0]][arr[1]];
-        if(L == m) {
-            ch2[arr[0]][arr[1]] = 1;
-            ch2[arr[1]][arr[0]] = 1;
-            b.add(arr[0]);
-            b.add(arr[1]);
-        }
-        else {
-            for (int i=1; i<=n; i++) {
-                if(ch[i] == 0) {
-                    ch[i] = 1;
-                    arr[L] = i;
-                    DFS(L+1, arr);
-                    arr[L] = 0;
-                    ch[i] = 0;
-                }
+    public void DFS(int L, int s) {
+        if (L == m) {
+            for (int x:combi) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for (int i = s; i <= n; i++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
             }
         }
-        return 0;
     }
 
 
@@ -37,12 +25,7 @@ public class 조합_구하기 {
         Scanner kb = new Scanner(System.in);
         n = kb.nextInt();
         m = kb.nextInt();
-        ch = new int[n + 1];
-        arr = new int[m];
-        ch2 = new int[35][35];
-        T.DFS(0, arr);
-        for (int i=0; i<b.size(); i+=2) {
-            System.out.println(b.get(i) + " " + b.get(i+1));
-        }
+        combi = new int[m];
+        T.DFS(0, 1);
     }
 }
