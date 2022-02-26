4441e1e4546e82037d64dcdd9207b5a546282827
seongwoo-choi <hibogo789@gmail.com>
2022-02-26 23:17:12 +0900

    fix

--- src/dfs_bfs/피자_배달_거리_DFS.java
+++ src/dfs_bfs/피자_배달_거리_DFS.java
@@ -1,4 +1,12 @@
 package dfs_bfs;
 
+import java.util.Scanner;
+
 public class 피자_배달_거리_DFS {
+
+    public static void main(String[] args) {
+        피자_배달_거리_DFS T = new 피자_배달_거리_DFS();
+        Scanner kb = new Scanner(System.in);
+
+    }
 }
