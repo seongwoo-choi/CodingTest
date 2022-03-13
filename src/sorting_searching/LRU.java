d2fda4e399984827daf20065e6088733a9c60f2c
seongwoo-choi <hibogo789@gmail.com>
2022-03-13 19:50:09 +0900

    LRU

---
+++ src/sorting_searching/LRU.java
@@ -0,0 +1,11 @@
+package sorting_searching;
+
+import java.util.Scanner;
+
+public class LRU {
+
+    public static void main(String[] args) {
+        LRU T = new LRU();
+        Scanner kb = new Scanner(System.in);
+    }
+}
